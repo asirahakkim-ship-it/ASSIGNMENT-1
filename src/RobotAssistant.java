4f3620f1a85681d6b449fe7c411c47825c536df8
LENOVO <asirahakkim@gmail.com>
2025-09-12 23:54:28 +0530

    JAVAJUNGLE ASSIST

--- src/RobotAssistant.java
+++ src/RobotAssistant.java
@@ -1,7 +1,18 @@
 public class RobotAssistant {
     public static void main(String[] args) {
         int a=10 ,b=5;
-        int op=1;
-        System.out.println(a+b);
+        int op=2;
+        if(op==1){
+            System.out.println(a+b);
+        }
+        else if(op==4){
+            System.out.println(a/b);
+        }
+        else if(op==2){
+            System.out.println(a-b);
+        }
+        else{
+            System.out.println(a*b);
+        }
     }
 }
