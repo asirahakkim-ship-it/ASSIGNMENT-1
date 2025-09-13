9565854da5726cd7f53f2a120719b298889c934a
LENOVO <asirahakkim@gmail.com>
2025-09-13 11:09:21 +0530

    JAVAJUNGLE ASSIST

---
+++ src/SpeedLimitCheck.java
@@ -0,0 +1,11 @@
+public class SpeedLimitCheck {
+    public static void main(String[] args) {
+        int speed=50;
+        if(speed>=60){
+            System.out.println("Overspeeding! Pay Fine");
+        }
+        else{
+            System.out.println("You are within the speed limit");
+        }
+    }
+}
