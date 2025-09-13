2e104a1c47987dd5b59cd8da233c2211f2ccb1fc
LENOVO <asirahakkim@gmail.com>
2025-09-13 12:04:10 +0530

    JAVAJUNGLE ASSIST

---
+++ src/PassOrFail.java
@@ -0,0 +1,11 @@
+public class PassOrFail {
+    public static void main(String[] args) {
+        int marks=80;
+        if(marks>=50){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
